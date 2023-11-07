2e3526f963d2b0b6b856e9bda89cb97d42c52ebe
ivan baktsheev <Ivan_Bakhcheev@epam.com>
2023-11-07 23:26:30 +0300

    fix: beeline smartbox turbo+ config works

--- parsers/beeline,smartbox-turbo-plus.sh
+++ parsers/beeline,smartbox-turbo-plus.sh
@@ -1,15 +1,19 @@
+
 get_oem_data_beeline__smartbox_turbo_plus () {
 
-	local part="factory"
+	local part="Factory"
 
-    wlan_ssid=$(mtd_get_cstring "$part" 0x21080)
+	wlan_phy_id=0
+	wlan_5ghz_phy_id=1
+
+	wlan_ssid=$(get_mtd_cstr "$part" 0x21080)
 	if [ "$wlan_ssid" != "${wlan_ssid#Beeline}" ] ; then
-		wlan_key=$(mtd_get_cstring "$part" 0x210a0)
+		wlan_key=$(get_mtd_cstr "$part" 0x210a0)
 
 		wlan_5ghz_ssid="${wlan_ssid:0:8}5${wlan_ssid:9}"
 		hostname="${wlan_ssid:0:8}${wlan_ssid:11}"
 
-		serial_number=$(mtd_get_cstring "$part" 0x21010)
+		serial_number=$(get_mtd_cstr "$part" 0x21010)
 		root_password="$serial_number"
 	else
 		wlan_ssid=
