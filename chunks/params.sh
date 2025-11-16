84570b9e809aa39eaf8154088440c084327c72eb
ivan baktsheev <Ivan_Bakhcheev@epam.com>
2025-11-16 21:55:51 +0300

    chore: preparing params to be parsed in js

--- chunks/params.sh
+++ chunks/params.sh
@@ -1,11 +1,11 @@
-# The country code determines WiFi channels and power level # String # Required
+# @type {string} wlan_country The country code determines WiFi channels and power level
 wlan_country="US"
 
-# WiFi encryption, WPA2 is the most compatible option (yet) # String # Required
+# @type {string} wlan_enc WiFi encryption, WPA2 is the most compatible option (yet)
 wlan_enc="psk2"
 
-# Turn on WiFi roaming (802.11r) feature
+# @type {0|1} [wlan_roaming=1] Turn on WiFi roaming (802.11r) feature
 # wlan_roaming=1
 
-# IP v4 router address for LAN interface (192.168.1.1)
+# @type {string} [ipv4_address] ipv4_address IP v4 router address for LAN interface (192.168.1.1)
 # ipv4_address="10.10.1.254"
