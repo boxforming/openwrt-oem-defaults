66546883997ff6f2341102299f234c852207c99d
ivan baktsheev <Ivan_Bakhcheev@epam.com>
2025-11-20 02:54:58 +0300

    chore: more params, US is too restrictive, WPA3 for 5GHz/6GHz

--- chunks/params.sh
+++ chunks/params.sh
@@ -1,11 +1,24 @@
-# @type {string} wlan_country The country code determines WiFi channels and power level
-wlan_country="US"
 
-# @type {string} wlan_enc WiFi encryption, WPA2 is the most compatible option (yet)
+# @type {string} wlan_country The country code determines WiFi channels and power level
+wlan_country="ES"
+
+# @type {string} wlan_enc WiFi encryption for 2.4GHz and also fallback https://openwrt.org/docs/guide-user/network/wifi/basic#encryption_modes
 wlan_enc="psk2"
 
-# @type {0|1} [wlan_roaming=1] Turn on WiFi roaming (802.11r) feature
-# wlan_roaming=1
+# @type {string} wlan_5ghz_enc WiFi encryption for 5GHz
+wlan_5ghz_enc="sae"
 
-# @type {string} [ipv4_address] ipv4_address IP v4 router address for LAN interface (192.168.1.1)
+# @type {string} wlan_6ghz_enc WiFi encryption for 6GHz
+wlan_6ghz_enc="sae"
+
+# @type {0|1} [wlan_roaming=0] Turn on WiFi roaming (802.11r) feature https://openwrt.org/docs/guide-user/network/wifi/basic#fast_bss_transition_options_80211r
+wlan_roaming=1
+
+# @type {string} [ipv4_address] IPv4 router address for LAN interface (192.168.1.1)
 # ipv4_address="10.10.1.254"
+
+# @type {string} [timezone] Device time zone (useful for 802.11v) https://github.com/openwrt/luci/blob/master/modules/luci-lua-runtime/luasrc/sys/zoneinfo/tzdata.lua
+timezone="CET-1CEST,M3.5.0,M10.5.0/3"
+
+# @type {string} [timezonename] Device time zone name
+timezonename="Europe/Madrid"
--- chunks/uci-defaults.sh
+++ chunks/uci-defaults.sh
@@ -16,10 +16,26 @@ if [[ -n $hostname ]] ; then
     # uci commit system
     echo "${hostname}" > /proc/sys/kernel/hostname
     /etc/init.d/system restart
-fi
+fi # $hostname
 
 if [[ -f /usr/bin/wireguard_watchdog ]] ; then
     echo '* * * * * /usr/bin/wireguard_watchdog' >> /etc/crontabs/root
 fi
+
+if [[ -n $ipv4_address ]] ; then
+	uci set network.lan.ipaddr="$ipv4_address"
+	uci set network.lan.netmask="${ipv4_netmask:-255.255.255.0}"
+	uci commit network
+	/etc/init.d/network restart
+fi # $ipv4_address
+
+if [[ -n $timezone ]] ; then
+    uci set system.@system[0].timezone="$timezone"
+    if [[ -n $timezonename ]] ; then
+        uci set system.@system[0].zonename="$timezonename"
+    fi
+fi # $timezone
+
+
 
 # your custom commands
